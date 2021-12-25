1f06f1bda8e1680ce0a0ba3c6b48a846369d7fcc
Khaled Galal <44265417+Gellesh@users.noreply.github.com>
2021-12-25 19:45:49 +0200

    add notFound component and handle route for it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-// import * as BooksAPI from './BooksAPI';
 import SearchPage from './components/SearchPage';
 import HomePage from './components/HomePage';
-import { Route } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 class BooksApp extends Component {
@@ -11,8 +11,11 @@ class BooksApp extends Component {
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/search" component={SearchPage} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/search" component={SearchPage} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>404</h1>
+        <h1>Page Not Found</h1>
+      </div>
+    </div>
+  );
+}
